a910673cc2f9fdffb3cd96223b48e02114d54b00
Dmytro Popov <lort20178@gmail.com>
2025-09-09 13:47:32 +0300

    Solution2

---
+++ src/makeAdder.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/***
+ * @return {function}
+ */
+function makeAdder() {
+  let sum = 0;
+
+  function adder(n) {
+    if (arguments.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+    sum += n;
+
+    return adder;
+  }
+
+  return adder;
+  // write code here
+}
+
+module.exports = makeAdder;
